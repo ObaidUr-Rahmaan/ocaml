ad86d3ac669b7e9242b5d986b336e761f1bdfc9b
ObaidUr-Rahmaan <33055080+ObaidUr-Rahmaan@users.noreply.github.com>
2019-09-13 07:54:58 +0100

    Update test_loop.ml

--- algorithms/test_loop.ml
+++ algorithms/test_loop.ml
@@ -1,4 +1,13 @@
 
+(*
+
+Tests if a directed graph given as input has at least one loop, i.e. a sequence of consecutive edges which reaches 
+back to its starting point. The graph is given as a list of edges, represented as pairs of nodes. For example one 
+of the representations a graph could be the list [(1,2); (1,3); (3,2); (3,4); (4;3)]. This graph has one loop, 3 -> 4 -> 3.
+
+*)
+
+
 let rec get_next x xys = List.filter (fun (x', y') -> x' = x) xys |> List.map snd
 
 let rec rem_dups = function
@@ -17,3 +26,6 @@ let rec move_fringe n xs xys =
 let test_loop xys =
   let xs = List.map fst xys |> List.sort_uniq compare in
   move_fringe (1 + List.length xs) xs xys
+  
+  
+  
