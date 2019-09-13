d52d4c7f16f69eda56f6b04b601c9e2655483698
ObaidUr-Rahmaan <33055080+ObaidUr-Rahmaan@users.noreply.github.com>
2019-09-13 07:46:34 +0100

    Update dblc.ml

--- functors/dblc.ml
+++ functors/dblc.ml
@@ -1,4 +1,4 @@
-module CircleSpec : DBLCIRCULAR = struct
+module CircularDoublyLinkedList : DBLCIRCULAR = struct
   exception Empty;;
 
   type 'a circular_cell = { mutable rev_cell : 'a circular_cell option; mutable value : 'a; mutable fwd_cell : 'a circular_cell option; };;
