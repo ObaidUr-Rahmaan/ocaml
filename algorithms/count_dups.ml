5a93a428858a9263fbda658f329aa42c39808ed1
ObaidUr-Rahmaan <33055080+ObaidUr-Rahmaan@users.noreply.github.com>
2019-09-13 07:53:40 +0100

    Update count_dups.ml

--- algorithms/count_dups.ml
+++ algorithms/count_dups.ml
@@ -1,3 +1,9 @@
+(*
+
+Counts the number of elements occurring in duplicates. If an element occurs more than twice then each occurrence counts.
+
+*)
+
 let count_dup xs =
   let rec aux xs = match xs with
    | [] -> 0
